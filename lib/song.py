16b7379044471152369d853a2b80618c03e2c5d0
dickson4954 <dicksonmurithi155@gmail.com>
2024-05-30 22:07:33 +0300

    initial commit

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,27 @@
 class Song:
+    genres=set()
+    artists=set()
+    
+    count = 0
+    def __init__(self, name, artist, genre):
+        self.name = name
+
+        self.artist = artist
+        self.genre = genre
+        self.genres.add(genre)
+        self.artists.add(artist)
+
+        Song.count += 1
+
+
+        Song.genre_count[genre] = Song.genre_count.get(genre, 0) + 1
+        Song.artist_count[artist] = Song.artist_count.get(artist, 0) + 1
+
+        
+        
+      
+        
+
+       
+        
     pass
